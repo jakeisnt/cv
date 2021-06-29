5eafb744ace4e3d26a2fa7394e5b1ffb45f0d1b9
Jacob Chvatal <jake@isnt.online>
2021-06-29 15:48:36 -0700

    finished with text based rendering target!

--- pollen/pollen.rkt
+++ pollen/pollen.rkt
@@ -1,6 +1,6 @@
 #lang racket/base
 
-(require racket/date racket/string txexpr pollen/setup net/url)
+(require racket/date racket/string racket/list txexpr pollen/setup net/url)
 (provide (all-defined-out))
 
 ;; setup submodule: provides define statements for values to override
@@ -23,7 +23,7 @@
 (define (heading . elements)
   (case (current-poly-target)
     [(ltx pdf) (apply string-append `("{\\huge " ,@elements "}"))]
-    [(txt) (map string-upcase elements)]
+    [(txt) (list "=== "(map string-upcase elements) " ===")]
     [(html) (txexpr 'h2 empty elements)]))
 
 ;; job title or designation; typically a subheader
@@ -33,19 +33,21 @@
     [(txt) `("*" ,@elements "*")]
     [(html) (txexpr 'h3 empty elements)]))
 
+(define (body . elements)
+  `("-------------------------\n" ,@elements))
+
+;; a social link with a service name and username provided by urlstring
+;; assumes form of e.g. http://servicename.com/i/j/k/username
 (define (link urlstr)
   (define urlstruct (string->url urlstr))
-  ;; (print urlstruct) */
   (define service (cadr (reverse (string-split (url-host urlstruct) "."))))
   (define username (path/param-path (car (reverse (url-path urlstruct)))))
-  (print service)
-  (print username)
-  (list service ": " username "  (" urlstr ")"))
+  (list "- " service ": " username "  (" urlstr ")"))
 
 (define (section . elements)
   (case (current-poly-target)
     [(ltx pdf) (apply string-append `("{\\huge " ,@elements "}"))]
-    [(txt) `("\n" ,@elements " ---")]
+    [(txt) `("\n" ,@elements "---")]
     [(html) (txexpr 'section empty elements)]))
 
 (define (emph . elements)
@@ -54,34 +56,71 @@
     [(txt) `("**" ,@elements "**")]
     [(html) (txexpr 'strong empty elements)]))
 
-;; 'experience' section
+;; an individual experience on the resume
 (define (experience . elements)
   (case (current-poly-target)
     [(ltx pdf) (apply string-append `("{\\bf " ,@elements "}"))]
     [(txt) `("--\n" ,@elements "\n")]
     [(html) (txexpr 'strong empty elements)]))
 
+;; the header of this experience
+;; typically encludes place, role and time period
 (define (exphead . elements)
   (case (current-poly-target)
     [(ltx pdf) (apply string-append `("{\\bf " ,@elements "}"))]
-    [(txt) `("" ,@elements "")]
+    [(txt)
+     (add-newlines (map (lambda (a) (list "|" a)) (rm-newlines elements)))
+     ]
     [(html) (txexpr 'strong empty elements)]))
 
+;; the body of an experience
+;; typically contains things accomplished during the experience
 (define (expbody . elements)
+  (case (current-poly-target)
+    [(ltx pdf) (apply string-append `("{\\bf " ,@elements "}"))]
+    [(txt) elements]
+    [(html) (txexpr 'strong empty elements)]))
+
+;; bullet point in the body of an experience
+(define (expbullet . elements)
   (case (current-poly-target)
     [(ltx pdf) (apply string-append `("{\\bf " ,@elements "}"))]
     [(txt) `("- " ,@elements "")]
     [(html) (txexpr 'strong empty elements)]))
 
+;; map but skip the last element of the list
+(define (mapca fn ls)
+  (cond
+    [(empty? (rest ls)) ls]
+    [(cons? ls) (cons (fn (first ls)) (mapca fn (rest ls)))]))
 
+;; remove newline strings from list
+(define (rm-newlines ls)
+  (filter (lambda (s) (not (and (string? s) (string=? s "\n")))) ls))
+
+;; add newline strings after every element in the list
+(define (add-newlines ls)
+  (foldr (lambda (e rst) (cons e (cons "\n" rst))) '() ls))
+
+;; a list of experiences, skills or otherwise
+(define (explist . elements)
+  (print elements)
+  ; (print (map (lambda (a) (string-append a " + ")) elements))) */
+  (mapca (lambda (a) (string-append a " + "))
+         (rm-newlines elements)))
+
+;; container for a sidebar (shown separately from a main body)
 (define (sidebar . elements)
   (case (current-poly-target)
     [(ltx pdf) (apply string-append `("{\\bf " ,@elements "}"))]
     [(txt) `("**" ,@elements "**")]
     [(html) (txexpr 'h3 empty elements)]))
 
+;; formatting for a date and time period
+;; should have enough information to display date however possible (racket datetime thing?)
 (define (date . elements)
   (case (current-poly-target)
     [(ltx pdf) (apply string-append `("{\\bf " ,@elements "}"))]
     [(txt) `("**" ,@elements "**")]
     [(html) (txexpr 'h3 empty elements)]))
+
