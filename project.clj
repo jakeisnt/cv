9aa359faa094671c32b948b60a8b5f211f7f8b6d
Jacob Chvatal <jake@isnt.online>
2022-07-05 10:08:39 -0400

    some issues with ifn?

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "cv.jacob.chvatal.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :main cv.core/main
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [quil "3.1.0"]])
