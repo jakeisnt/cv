36467128d61c974135c4e74710434cac9f79be8a
Jacob Chvatal <jake@isnt.online>
2022-07-05 10:07:36 -0400

    update docs

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject cv "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Jake's CV and document generation system!"
+  :url "cv.jacob.chvatal.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
