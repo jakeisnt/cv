3129173bf5fbb9ff73d4408a3725d9aece468c0f
Jacob Chvatal <jake@isnt.online>
2022-07-08 21:35:04 -0400

    supposedly now set up for pdf generation

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "cv.jacob.chvatal.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :main cv.core/main
+  :main "cv.core"
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [quil "3.1.0"]])
+                 [quil "3.1.0"]
+                 [quil/processing-pdf "3.5.4"]])
--- src/cv/core.clj
+++ src/cv/core.clj
@@ -2,50 +2,45 @@
   (:require [quil.core :as q]
             [quil.middleware :as m]))
 
-(defn setup []
-  ; Set frame rate to 30 frames per second.
-  (q/frame-rate 30)
-  ; Set color mode to HSB (HSV) instead of default RGB.
-  (q/color-mode :hsb)
-  ; setup function returns initial state. It contains
-  ; circle color and position.
-  {:color 0
-   :angle 0})
+(defn mm-to-px
+  "Convert millimeters to pixels"
+  [mm]
+  (/ (* mm 72) 25.4))
 
-(defn update-state [state]
-  ; Update sketch state by changing circle color and position.
-  {:color (mod (+ (:color state) 0.7) 255)
-   :angle (+ (:angle state) 0.1)})
+(defn in-to-mm
+  "Convert inches to millimeters"
+  [in]
+  (* in 25.4))
 
-(defn draw-state [state]
-  ; Clear the sketch by filling it with light-grey color.
-  (q/background 240)
-  ; Set circle color.
-  (q/fill (:color state) 255 255)
-  ; Calculate x and y coordinates of the circle.
-  (let [angle (:angle state)
-        x (* 150 (q/cos angle))
-        y (* 150 (q/sin angle))]
-    ; Move origin point to the center of the sketch.
-    (q/with-translation [(/ (q/width) 2)
-                         (/ (q/height) 2)]
-      ; Draw the circle.
-      (q/ellipse x y 100 100))))
+(defn in-to-px
+  "Convert inches to pixels"
+  [in]
+  (mm-to-px (in-to-mm in)))
+
+(comment
+  (let [xs (q/available-fonts)]
+    (first (filter #(re-find #"(?i)" %) xs))))
 
 
-(defn cv-sketch []
-  (q/defsketch cv
-    :title "You spin my circle right round"
-    :size [500 500]
-    ; setup function called only once, during sketch initialization.
-    :setup setup
-    ; update-state is called on each iteration before draw-state.
-    :update update-state
-    :draw draw-state
-    :features [:keep-on-top]
-    ; This sketch uses functional-mode middleware.
-    ; Check quil wiki for more info about middlewares and particularly
-    ; fun-mode.
-    :middleware [m/fun-mode]))
+(def a4 {:width (mm-to-px 210)
+         :height (mm-to-px 297)})
 
-(defn main [] (cv-sketch))
+(def us-paper {:width (in-to-px 8.5)
+               :height (in-to-px 11)})
+
+(defn draw! []
+  (q/text-mode :shape)
+  (q/text-font (q/create-font "3270 Narrow Nerd Font Complete" 20))
+  (q/text-align :left :top)
+  (q/text "hello! this is some test text.!" 50 400) ;; Address card
+  (q/text-mode :model))
+;; reset back to :model if you wanted.  )
+
+(q/sketch
+ :draw (fn []
+         (q/do-record (q/create-graphics (:width a4) (:height a4) :pdf "out.pdf")
+                      (draw!))
+         (q/exit)))
+
+
+(defn -main [& args])
